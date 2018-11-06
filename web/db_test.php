fb3675b615ee93c0f97b01bb1d2a90289ca04450
peter.chen <pencil722@gmail.com>
2018-11-07 07:53:01 +0800

    db test

--- web/db_test.php
+++ web/db_test.php
@@ -11,3 +11,22 @@ var_dump('this is a db test');
 $mysql_connect_str = getenv('CLEARDB_DATABASE_URL');
 
 var_dump($mysql_connect_str);
+
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$conn = new mysqli($server, $username, $password, $db);
+$sql ="SELECT * FROM `line_messages_records`";
+if($result = $conn->query($sql)) {
+    foreach($result as $row) {
+//...
+    }
+} else {
+    throw new Exception($conn->error);
+}
+
+var_dump('db connect finish');
